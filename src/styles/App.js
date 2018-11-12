7fd77d26cfc91f72bce44cdefeb88c1ad06c3436
Sivasubramanyam A <ssdragonar5@gmail.com>
2018-11-13 00:30:29 +0530

    Allow refreshing launch data

---
+++ src/styles/App.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+import { Constants } from 'expo';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#142634'
+  },
+  rocket: {
+    fontSize: 24,
+    color: '#d48872'
+  },
+  launchPad: {
+    marginTop: 32,
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#bdc7d1'
+  },
+  helperText: {
+    marginTop: 12,
+    fontSize: 14,
+    textAlign: 'center',
+    color: '#5a666b'
+  }
+});
